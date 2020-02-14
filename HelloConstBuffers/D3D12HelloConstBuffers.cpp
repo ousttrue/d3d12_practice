d0297c52336acdb33d165c86547378c5643a36f1
ousttrue <ousttrue@gmail.com>
2020-02-14 11:19:12 +0900

    separate d3dh1lper.h

--- HelloConstBuffers/D3D12HelloConstBuffers.cpp
+++ HelloConstBuffers/D3D12HelloConstBuffers.cpp
@@ -6,88 +6,12 @@
 #include <d3dcompiler.h>
 #include <stdexcept>
 #include "d3dx12.h"
+#include "d3dhelper.h"
 
 std::string g_shaders =
 #include "shaders.hlsl"
     ;
 
-template <class T>
-using ComPtr = Microsoft::WRL::ComPtr<T>;
-
-static UINT GetDxgiFactoryFlags()
-{
-    UINT dxgiFactoryFlags = 0;
-
-#if defined(_DEBUG)
-    // Enable the debug layer (requires the Graphics Tools "optional feature").
-    // NOTE: Enabling the debug layer after device creation will invalidate the active device.
-    {
-        ComPtr<ID3D12Debug> debugController;
-        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-        {
-            debugController->EnableDebugLayer();
-
-            // Enable additional debug layers.
-            dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-        }
-    }
-#endif
-
-    return dxgiFactoryFlags;
-}
-
-// Helper function for acquiring the first available hardware adapter that supports Direct3D 12.
-// If no such adapter can be found, *ppAdapter will be set to nullptr.
-static ComPtr<IDXGIAdapter1> GetHardwareAdapter(IDXGIFactory2 *pFactory)
-{
-    ComPtr<IDXGIAdapter1> adapter;
-    for (UINT adapterIndex = 0; DXGI_ERROR_NOT_FOUND != pFactory->EnumAdapters1(adapterIndex, &adapter); ++adapterIndex)
-    {
-        DXGI_ADAPTER_DESC1 desc;
-        adapter->GetDesc1(&desc);
-
-        if (desc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE)
-        {
-            // Don't select the Basic Render Driver adapter.
-            // If you want a software adapter, pass in "/warp" on the command line.
-            continue;
-        }
-
-        // Check to see if the adapter supports Direct3D 12, but don't create the
-        // actual device yet.
-        if (SUCCEEDED(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, _uuidof(ID3D12Device), nullptr)))
-        {
-            return adapter;
-        }
-    }
-
-    return nullptr;
-}
-
-inline std::string HrToString(HRESULT hr)
-{
-    char s_str[64] = {};
-    sprintf_s(s_str, "HRESULT of 0x%08X", static_cast<UINT>(hr));
-    return std::string(s_str);
-}
-
-class HrException : public std::runtime_error
-{
-public:
-    HrException(HRESULT hr) : std::runtime_error(HrToString(hr)), m_hr(hr) {}
-    HRESULT Error() const { return m_hr; }
-
-private:
-    const HRESULT m_hr;
-};
-
-inline void ThrowIfFailed(HRESULT hr)
-{
-    if (FAILED(hr))
-    {
-        throw HrException(hr);
-    }
-}
 
 class Impl
 {
